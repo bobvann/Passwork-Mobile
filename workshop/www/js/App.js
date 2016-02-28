52bcc700dea0c1a75f0f9fa521b91147d94b8619
piero ometto <info@pieroometto.com>
2016-02-28 01:21:13 +0100

    style

--- workshop/www/js/App.js
+++ workshop/www/js/App.js
@@ -73,7 +73,7 @@ App.onDeviceReady = function(){
     console.log("Device Ready");
 
     if(Utils.getDevice()==Utils.DEVICES.IOS){
-        $(".navbar").css("padding-top","15px");
+        $(".navbar").css("padding-top","55px");
         $(".navbar-brand").css("top","15px");
     }
 
